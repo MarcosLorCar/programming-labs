bf0f59b9e0ae82a7f0e57ca40ec4750b87dcb3b5
Rafa <dismas5098@gmail.com>
2024-11-11 20:16:05 +0100

    nueva version

--- Rafa/dimesions.java
+++ Rafa/dimesions.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class dimesions {
     private static final Scanner KEYBOARD = new Scanner(System.in);
     public static void main(String[] args) {
-        int adult, minor, option, oPtionp;
+        int ticket, adult, minor, option, oPtionp, row, column;
+        int pSeats = 1;
         float price, fp1;
         char seats[][] = new char[5][4];
         int aSeats = seats.length * seats[0].length;
@@ -24,39 +25,89 @@ public class dimesions {
         System.out.println("There are currently available these number of seats: " + aSeats + "\n" );
         System.out.println("Choose an option please: \n" + "1. Purchase tickets \n" + "2. Change tickets \n" + "3. Show seat status \n" + "4. Exit the program");
         option = KEYBOARD.nextInt();
-        if (option == 1) {
-            System.out.println("Choose a number of tickets greater than one, we remember you we have " + aSeats + " seats available");
-            adult = KEYBOARD.nextInt();
-            System.out.println("Choose a number of tickets for minors");
-            minor = KEYBOARD.nextInt();
-            System.out.println("Choose a ticket purchase option: \n" + "1. Manual selection \n" + "2. Automatic selection \n" + "3. Automatic selection of available seats \n" + "4. Exit purchase program");
-            oPtionp = KEYBOARD.nextInt();
-            switch (oPtionp) {
+        while (option != 4) {
+            switch (option) {
                 case 1:
+                System.out.println("Choose a number of tickets greater than one, we remember you we have " + aSeats + " seats available");
+                adult = KEYBOARD.nextInt();
+                System.out.println("Choose a number of tickets for minors");
+                minor = KEYBOARD.nextInt();
+                if (adult == minor) {
+                    System.out.println("ERROR. We need at least one adult");
+                    System.out.println("Choose a number of tickets greater than one, we remember you we have " + aSeats + " seats available");
+                    adult = KEYBOARD.nextInt();
+                    System.out.println("Choose a number of tickets for minors");
+                    minor = KEYBOARD.nextInt();
                     
+                }
+                ticket = adult;
+                System.out.println("Choose a ticket purchase option: \n" + "1. Manual selection \n" + "2. Automatic selection \n" + "3. Automatic selection of available seats \n" + "4. Exit purchase program");
+                oPtionp = KEYBOARD.nextInt();
+                switch (oPtionp) {
+                    case 1:
+                        while (pSeats <= ticket) {
+                            System.out.println("Please, pick a row");
+                            row = KEYBOARD.nextInt();
+                            row--;
+                            System.out.println("Please, pick a column");
+                            column = KEYBOARD.nextInt();
+                            column--;
+                            if ((row < seats.length && row >= 0) && (column < seats[0].length && column >= 0)){
+                                if (seats[row][column] == 'A') {
+                                     seats[row][column] = 'O';
+                                     pSeats++;
+                                     System.out.println("Your seat has been assigned");
+                                }
+                                else {
+                                    System.out.println("ERROR. That seat is occupied, choose another");
+                                }
+                            }
+                            else{
+                                System.out.println("ERROR. The size doesn't match the seats");
+                            }
+                        }
+                        break;
+                    case 2:
+                        while (pSeats <= ticket) {
+                            for (int i = 0; i < seats.length && pSeats <= ticket; i++) {
+                                for (int j = 0; j < seats[i].length && j < ticket; j++) {
+                                    if (seats[i][j]=='A') {
+                                        seats[i][j]='O';
+                                        pSeats++;
+                                        System.out.printf("your seat in row %d and column %d has been selected \n", i+1,j+1);
+                                    }
+                                }
+                            }
+                        }
+                        System.out.println("your seats has been assigned");
+                        break;
+                    case 3:
+    
+                        break;
+                    case 4:
+                        System.out.println("Thanks for using our services \n");
+                        System.out.println("There are currently available these number of seats: " + aSeats + "\n" );
+                        System.out.println("Choose an option please: \n" + "1. Purchase tickets \n" + "2. Change tickets \n" + "3. Show seat status \n" + "4. Exit the program");
+                        option = KEYBOARD.nextInt();
+                        break;
+                    default:
+                        System.out.println("ERROR. That option is not in the program");
+                        option = KEYBOARD.nextInt();
+                        break;
+                }
                     break;
                 case 2:
-
+    
                     break;
                 case 3:
-
+    
                     break;
                 case 4:
-
+    
                     break;
                 default:
-
                     break;
             }
-        }
-        if (option == 2) {
-            
-        }
-        if (option == 3) {
-            
-        }
-        if (option == 4) {
-            
         }
     }
 }
